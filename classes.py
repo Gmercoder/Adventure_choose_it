0b5772543ab6c970e69d567c1e2b8e5d37258a32
da-verge <vterrel00@gmail.com>
2025-10-09 11:49:08 -0500

    Create classes.py

---
+++ classes.py
@@ -0,0 +1,21 @@
+import random as rand
+import time as ti
+
+class character():
+  int strength
+  int dex
+  int constitiution
+  int intellegence
+  int wisdom
+  int resistance
+  
+  int sanity
+  int health
+  int money
+  
+  str name
+  str clas
+  str weapon
+  
+  list weaknesses
+  list inventory
