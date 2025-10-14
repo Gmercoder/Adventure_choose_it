7fe24ca0085b303d591c9186bac6c7f733d68c77
da-verge <vterrel00@gmail.com>
2025-10-14 11:03:20 -0500

    Update monsters.py

--- monsters.py
+++ monsters.py
@@ -1,1 +1,65 @@
+class monster():
+  class chimera():
+
+  class skeleton():
+
+  class zombie():
+
+  class dragon():
+
+  class hydra():
+
+  class slime():
+
+  class ghost():
+
+  class ghoul():
+
+  class basilisk():
+
+  class cyclops():
+
+  class gorgon():
+
+  class griffin():
+
+  class minotaur():
+
+  class sphinx():
+
+  class banshee():
+
+  class bonless():
+
+  class wraith():
+
+  class lich():
+
+  class mummy():
+
+  class necromanser():
+
+  class vampire():
+
+  wyvern
+
+  Oblex
+
+  beholder
+
+  manticore
+
+  cockatrice
+
+  werewolf
+
+  giant
+
+  goblin
+
+  orc
+
+  troll
+
+  ogre
 
