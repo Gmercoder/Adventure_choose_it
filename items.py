580afedb586658d9a49a818f0bdb21d89aee1d9f
da-verge <vterrel00@gmail.com>
2025-10-15 11:23:08 -0500

    Create items.py

---
+++ items.py
@@ -0,0 +1,18 @@
+class Misc_item():
+  int value
+  str name
+  int weight
+
+class Weapon():
+  str skill
+  int weight
+  int damage
+  str name
+  str value
+  str type
+
+class Armor():
+  str name
+  int weight
+  int value
+  int armorClass
