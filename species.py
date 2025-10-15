9125be049af632cd92c4ebeefd5ce4854ae984de
da-verge <vterrel00@gmail.com>
2025-10-15 11:02:40 -0500

    Update species.py

--- species.py
+++ species.py
@@ -1,1 +1,33 @@
+class Human():
+  int skill_modifier
+  int skill_modifier
 
+class Dragonborne():
+  int skill_modifier
+  int skill_modifier
+
+class Dwarf():
+  int skill_modifier
+  int skill_modifier
+
+class Elf():
+
+  class high():
+    int skill_modifier
+    int skill_modifier
+  
+  class wood():
+    int skill_modifier
+    int skill_modifier
+  
+  class dark():
+    int skill_modifier
+    int skill_modifier
+
+class Halfling():
+  int skill_modifier
+  int skill_modifier
+
+class half-elf():
+  int skill_modifier
+  int skill_modifier
