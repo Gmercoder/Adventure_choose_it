c1faa9d6d706e3183204b86fd8fcfbbfafd353c0
da-verge <vterrel00@gmail.com>
2025-10-16 11:40:46 -0500

    Add Magic_Armor and Magic_Weapon classes

--- items.py
+++ items.py
@@ -23,3 +23,26 @@ class Weapon:
 
     def damage(self, character)->int:
         return self.damage_dice.roll() + character.__getattribute__(self.damage_attribute) % 4
+
+class Magic_Armor:
+    def __init__(self, name:str, armor_class:int, value:int, weight:int, abilities:list) -> None:
+        self.name = name
+        self.armor_class = armor_class
+        self.value = value
+        self.weight = weight
+        self.abilities = abilities
+
+    def damage_resistance():
+        
+
+
+class Magic_Weapon:
+    def __init__(self, name:str, damage_dice:Dice, damage_attribute:str, value:int, weight:int) -> None:
+        self.name = name
+        self.damage_dice = damage_dice
+        self.damage_attribute = damage_attribute
+        self.value = value
+        self.weight = weight
+
+    def damage(self, character)->int:
+        return self.damage_dice.roll() + character.__getattribute__(self.damage_attribute) % 4
