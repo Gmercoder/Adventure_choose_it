6c3c67c6b1b587a5f60d01ae098ddaa2ba07165e
da-verge <vterrel00@gmail.com>
2025-10-16 11:29:14 -0500

    Add constructors to Misc_item, Weapon, and Armor classes
    Refactor item classes to use constructors for initialization.

--- items.py
+++ items.py
@@ -1,18 +1,25 @@
 class Misc_item():
-  int value
-  str name
-  int weight
+    def __init__(self, name:str, value:int, weight:int) -> None:
+        self.name = name
+        self.value = value
+        self.weight = weight
 
-class Weapon():
-  str skill
-  int weight
-  int damage
-  str name
-  str value
-  str type
 
-class Armor():
-  str name
-  int weight
-  int value
-  int armorClass
+class Armor:
+    def __init__(self, name:str, armor_class:int, value:int, weight:int) -> None:
+        self.name = name
+        self.armor_class = armor_class
+        self.value = value
+        self.weight = weight
+
+
+class Weapon:
+    def __init__(self, name:str, damage_dice:Dice, damage_attribute:str, value:int, weight:int) -> None:
+        self.name = name
+        self.damage_dice = damage_dice
+        self.damage_attribute = damage_attribute
+        self.value = value
+        self.weight = weight
+
+    def damage(self, character)->int:
+        return self.damage_dice.roll() + character.__getattribute__(self.damage_attribute) % 4
