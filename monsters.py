f0b31595bbd9361d9f45003944ced6b3dde4e92f
Gmercoder <nicoram432@gmail.com>
2025-10-30 11:16:51 -0500

    Fix typos in monster attributes and abilities

--- monsters.py
+++ monsters.py
@@ -6,13 +6,13 @@ Skeleton = charz(strength = 10, dexterity = 9, health = 30, armor = rusted, weap
 
 Zombie = charz(strength = 9, dexterity = 8, health = 20, armor = none, weapons = [teeth, hands], num_attacks = 2, playable = false, xp = 10, weakness = [weapons, magic])
 
-Dragon = charz(strength = 18, dexterity = 19, health = 235, armor = Natural, weapons = [talons, tail, bite], num_attacks = 4, playable = false, xp = 1000, weakness = [magical, cold, underbelly], abilties = [fire breathing])
+Dragon = charz(strength = 18, dexterity = 19, health = 235, armor = Natural, weapons = [talons, tail, bite], num_attacks = 4, playable = false, xp = 1000, weakness = [magical, cold, underbelly], abilties = [fire_breathing])
 
-Hydra = charz(strength = 17, dexterity = 18, con = 15, health = 215, armor = Natural, weapons = [talons, tail, bite], num_attacks = 3, playable = false, xp = 1100, weakness = [magical, fire, cold, weapons], abilities = [regeneration, fire breathing])
+Hydra = charz(strength = 17, dexterity = 18, con = 15, health = 215, armor = Natural, weapons = [talons, tail, bite], num_attacks = 3, playable = false, xp = 1100, weakness = [magical, fire, cold, weapons], abilities = [regeneration, fire_breathing])
 
 Green_slime = charz(strength = 10, dexterity = 9, con = 10, health = 15, armor = Natural, weapons = [pseudopod], num_attacks = 1, playable = false, xp = 15, weakness = [magical, lightning, cold], abilities = [dissolve_weapons])
 
-class Ghost():
+Ghost = charz(strength = 10, dexterity = 9, con = 10, health = 50, armor = Ethereal, weapons = [spirit_dagger, magic_dagger], num_attacks = 2, playable = false, xp = 20, weakness = [magic_weapons, holy_water], abilities = [intangibility] 
 
 class Ghoul():
 
